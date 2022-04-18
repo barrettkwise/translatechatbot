f784452c86c1f523dd9c5ca33c96d643e2927707
barrett wise <barrettkwise@gmail.com>
2022-04-18 14:03:44 -0400

    Update app.bat

--- anvilversion/app.bat
+++ anvilversion/app.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-cd E:\school\"all programming files"\translatechatbot\anvilversion
+cd C:\Users\%USERNAME%\Downloads\translatechatbot-master\anvilversion
 start ./translatechat.html
 py -i localanvil.py
 pause
